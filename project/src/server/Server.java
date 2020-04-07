760ab78c035292b60551f632fecc634b12e2504b
magnubau <magnubau@stud.ntnu.no>
2020-04-07 15:53:53 +0200

    Added a shared list of connected clients

--- project/src/server/Server.java
+++ project/src/server/Server.java
@@ -4,9 +4,22 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+/**
+ * This class handles connection requests from clients and creates
+ * a new instance of ClientHandler for each new client, and adds it
+ * a shared list
+ * @author magnubau
+ */
 public class Server {
+    /**
+     *This main method goes in loops waiting for new connections to
+     * handle
+     *
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         final int PORTNR = 3000;
+        int id = 0;
 
         ServerSocket tjener = new ServerSocket(PORTNR);
         List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<ClientHandler>());
@@ -14,10 +27,11 @@ public class Server {
 
         while(true){
             Socket connection = tjener.accept();
-            Thread clientHandler = new ClientHandler(connection);
+            Thread clientHandler = new ClientHandler(connection, id);
             clients.add(clientHandler);
             clientHandler.start();
-            System.out.println(clients.size());
+            System.out.println("client connected.\nClient id: " + id + "\nNumber of clients: " + clients.size() + "\n");
+            id++;
         }
     }
 }
