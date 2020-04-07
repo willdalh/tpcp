935071aa1df4165a063007d06e84c23b248e52ac
magnubau <magnubau@stud.ntnu.no>
2020-04-07 15:09:37 +0200

    Made the Server class

--- project/src/server/Server.java
+++ project/src/server/Server.java
@@ -1,4 +1,23 @@
 package server;
 
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        final int PORTNR = 3000;
+
+        ServerSocket tjener = new ServerSocket(PORTNR);
+        List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<ClientHandler>());
+        System.out.println("Server is running. Waiting for clients...");
+
+        while(true){
+            Socket connection = tjener.accept();
+            Thread clientHandler = new ClientHandler(connection);
+            clients.add(clientHandler);
+            clientHandler.start();
+            System.out.println(clients.size());
+        }
+    }
 }
