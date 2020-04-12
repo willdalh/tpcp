ca83c3bf337615e35cf08e70204ecb4fa27ef2be
William Dalheim <williad@stud.ntnu.no>
2020-04-12 14:12:45 +0200

    Added code to Participant.java

--- project/src/client/Client.java
+++ project/src/client/Client.java
@@ -7,6 +7,5 @@ public class Client {
     public static void main(String[] args) {
         Participant participant = new Participant();
         participant.startConnection();
-        System.out.println("Hello");
     }
 }
---
+++ project/src/utils/Utils.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Utils {
+    public static void printStringArray(String[] arr){
+        for (int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
