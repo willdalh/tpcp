1989d416f5fd3112e1d4bba22fc0e23ffa53a076
magnubau <magnubau@stud.ntnu.no>
2020-04-18 11:56:37 +0200

    working on tests

--- project/test/server/ClientHandlerTest.java
+++ project/test/server/ClientHandlerTest.java
@@ -2,6 +2,10 @@ package server;
 
 import org.junit.jupiter.api.*;
 import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,18 +13,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ClentHandlerTest {
 
+    Socket mockSocket;
     ClientHandler clientHandler;
 
     @BeforeEach
-    void setUp() {
-        final Socket socket = mock(Socket.class);
-        this.clientHandler = new ClientHandler(socket, 1);
+    void setUp() throws IOException {
+        mockSocket = mock(Socket.class);
+        when(mockSocket.getInputStream()).thenReturn(mock(InputStream.class));
+        when(mockSocket.getOutputStream()).thenReturn(mock(OutputStream.class));
+        this.clientHandler = new ClientHandler(mockSocket, 1);
+    }
+
+    @AfterEach
+    void tearDown(){
+        mockSocket = null;
+        clientHandler = null;
     }
 
     @Test
     void sendToParticipant(){
         boolean messageSent = clientHandler.sendToParticipant("hei");
         assertTrue(messageSent = true, "Message was not sent");
+    }
+
+    @Test
+    void readFromParticipant() throws IOException {
+        when(clientHandler.reader.readLine()).thenReturn("hei");
+        assertEquals("hei", clientHandler.readFromParticipant());
     }
 
 }
